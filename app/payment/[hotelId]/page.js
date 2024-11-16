759e1fd8178b08c3731e6d348e64a375302fa967
rajib <rajibrad@gmail.com>
2024-11-16 13:41:20 +0600

    payment page done

---
+++ app/payment/[hotelId]/page.js
@@ -0,0 +1,21 @@
+import PaymentForm from '@/components/payment/PaymentForm';
+import React from 'react';
+
+const PaymentPage = async (params) => {
+    const { hotelId } = await params
+    console.log(hotelId);
+    return (
+        <section class="container">
+
+            <div class="p-6 rounded-lg max-w-xl mx-auto my-12 mt-[100px]">
+                <h2 class="font-bold text-2xl">Payment Details</h2>
+                <p class="text-gray-600 text-sm">You have picked <b>Effotel By Sayaji Jaipur</b> and base price is <b>$10</b>
+                </p>
+                <PaymentForm />
+            </div>
+        </section>
+    );
+};
+
+export default PaymentPage
+    ;
