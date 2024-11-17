27d9819edadf8bd6e0e14659c37f680b4cc6fd0d
rajib <rajibrad@gmail.com>
2024-11-17 13:42:04 +0600

    create user login system with email and pass credential

--- auth.js
+++ auth.js
@@ -1,7 +1,9 @@
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials"
 import mongoClientPromise from "./database/mongoClientPromise";
+import { Users } from "./models/users-model";
 
 
 export const {
@@ -11,7 +13,37 @@ export const {
     signOut,
 } = NextAuth({
     adapter: MongoDBAdapter(mongoClientPromise, { databaseName: process.env.ENVIROMENT }),
+    session: {
+        strategy: 'jwt'
+    },
     providers: [
+        CredentialsProvider({
+            credentials: {
+                email: {},
+                password: {}
+            },
+
+            async authorize(credentials) {
+                if (credentials === null) return null
+                try {
+                    const user = await Users.findOne({ email: credentials?.email })
+                    if (user) {
+                        const isMatched = user.email === credentials.email
+                        if (isMatched) {
+                            return user
+                        } else {
+                            throw new Error("Email or Password mismatch")
+                        }
+
+                    } else {
+                        throw new Error("User Not Found")
+                    }
+
+                } catch (error) {
+                    throw new Error(error)
+                }
+            }
+        }),
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
